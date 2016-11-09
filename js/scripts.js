c9b79315f7b3a083b6f01f5b72a17e60ab4ecf47
Konrad <konrad.bolesta@gmail.com>
2016-11-09 11:24:24 +0100

    conditionals

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var a = 3
+    b = 4
+    value = (a*a) + (2 * a * b) - (b*b)
+console.log(value)
+
+if (value < 0) {
+    console.log('Wynik ujemny');
+}
+
+else if (value = 0){
+    console.log('Wynik wynosi zero');
+}
+
+else {
+    console.log('Wynik dodatni');
+}
