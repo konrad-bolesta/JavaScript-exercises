28b03d67d301d2d0f24090358bf6f3e671083548
Konrad <konrad.bolesta@gmail.com>
2016-11-14 21:48:05 +0100

    fix functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-var triangle1Area = getTriangleArea(10, 15),
-    triangle2Area = getTriangleArea(5, 8),
-    triangle3Area = getTriangleArea(12, 20);
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(5, 8);
+var triangle3Area = getTriangleArea(12, 20);
 
 function getTriangleArea(a, h) {
-    if ((a<=0) || (h<=0)) {
+    if (a <= 0 || h <= 0) {
         console.log('Nieprawidłowe dane');
+        return;
     }
-    return a*h/2;
+    return a * h / 2;
 }
 
 console.log('Pole pierwszego trójkąta: ' + triangle1Area);
