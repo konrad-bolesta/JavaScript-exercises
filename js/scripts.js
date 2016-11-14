dc03b5f217d4161ef90d32a7c1c71af7f3b67e09
Konrad <konrad.bolesta@gmail.com>
2016-11-14 22:10:38 +0100

    events

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+﻿var list = document.getElementById('Lista');
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+    list.innerHTML += '<li></li>';
+    var itemsByTagName = document.getElementsByTagName('li');
+
+    for (var i = 1; i < itemsByTagName.length; i++) {
+    itemsByTagName[i].innerHTML = "item" + " " + i;
+    }
+});
