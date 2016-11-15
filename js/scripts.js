d4476566c7aaab8797858a58d4928b0cb685c41a
Konrad <konrad.bolesta@gmail.com>
2016-11-15 21:36:07 +0100

    fix events

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,7 @@
 var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
-    list.innerHTML += '<li></li>';
-    var itemsByTagName = document.getElementsByTagName('li');
-
-    for (var i = 1; i < itemsByTagName.length; i++) {
-    itemsByTagName[i].innerHTML = "item" + " " + i;
-    }
+    var newLi = document.createElement("li");
+    newLi.innerHTML = 'item' + " " + document.getElementsByTagName('li').length;
+    list.appendChild(newLi);
 });
